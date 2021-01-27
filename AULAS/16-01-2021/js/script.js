1955b56a0abd01462fe76d51b521a8acbffec7b1
Alain Camacho <alain.mcf@gmail.com>
2021-01-26 21:04:49 -0300

    Cria a aula 16/01/2021

---
+++ AULAS/16-01-2021/js/script.js
@@ -0,0 +1,7 @@
+// $(".alert") //Pegando o elemento pela classe
+$("#alerta") //Pegando o elemento pela id
+
+// document.getElementById("alerta").innerHTML = "Olá JQuery";
+$("#alerta").text("Ola JQuery!");
+$("#alerta").removeClass("alert-primary");
+$("#alerta").addClass("alert-warning");
